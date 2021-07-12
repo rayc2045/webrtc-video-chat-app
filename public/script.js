274b7551576d3b2fb267761865bbeafdb03fb34e
Ray C <rayc2045@gmail.com>
2021-07-12 22:13:03 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -7,7 +7,7 @@ const myPeer = new Peer(undefined, {
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 
-const peers = {}
+const peers = {};
 navigator.mediaDevices
   .getUserMedia({
     video: true,
@@ -18,14 +18,16 @@ navigator.mediaDevices
     myPeer.on('call', call => {
       call.answer(stream);
       const video = document.createElement('video');
-      call.on('stream', userVideoStream => addVideoStream(video, userVideoStream));
+      call.on('stream', userVideoStream =>
+        addVideoStream(video, userVideoStream)
+      );
     });
     socket.on('user-connected', userId => connectToNewUser(userId, stream));
   });
 
 socket.on('user-disconnected', userId => {
   if (peers[userId]) peers[userId].close();
-})
+});
 
 myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id);
